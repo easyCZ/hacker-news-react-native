23e25b66d5a253d76e74cb50e348362c4588f0f5
Milan Pavlik <pavlik.mil@gmail.com>
2015-05-25 22:46:40 +0100

    Re-init

--- HackerNewsRCTTests/HackerNewsRCTTests.m
+++ HackerNewsRCTTests/HackerNewsRCTTests.m
@@ -17,11 +17,11 @@
 #define TIMEOUT_SECONDS 240
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface HackerNewsTests : XCTestCase
+@interface HackerNewsRCTTests : XCTestCase
 
 @end
 
-@implementation HackerNewsTests
+@implementation HackerNewsRCTTests
 
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
